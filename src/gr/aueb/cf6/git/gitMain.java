187b73184ee914ec486ed3b00b9e3c1148be7d82
Simos Esnafidis <simos.esnafidis@gmail.com>
2024-11-05 19:59:59 +0200

    gitMain java pushed, finally after correcting errors

---
+++ src/gr/aueb/cf6/git/gitMain.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf6.git;
+
+public class gitMain {
+    public static void main(String[] args) {
+        // system.out.println doesn't seem to be working, so I am using this comment
+    }
+}
